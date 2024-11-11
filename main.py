07234a97d02efcd17064ff075b03dcf424437da1
Subhechha <gautamsubhechha@gmail.com>
2024-11-11 10:09:11 +0545

    hgjhgj

---
+++ main.py
@@ -0,0 +1,5 @@
+import mathematics # import whole packages
+from mathematics import trigonometry
+cosine_of_45 = mathematics.trigonometry.cosine(45)
+trigonometry.cosine(45)
+print(cosine_of_45)
---
+++ math_utls.py
@@ -0,0 +1,6 @@
+# maths_utils.py
+def add(a,b):
+    return a+b
+
+def subtract(a,b):
+    return a-b
